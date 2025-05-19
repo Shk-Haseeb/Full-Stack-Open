2bffde3209fc52a0dcda8212692239f1c07f657b
Haseeb <haseeb.shaikh@helsinki.fi>
2025-05-19 15:22:34 +0300

    Exercises 2.1-2.5 completed

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ total }) => <p><strong>Total of {total} exercises</strong></p>
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+export default Course
