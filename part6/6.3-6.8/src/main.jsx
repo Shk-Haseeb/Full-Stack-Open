e6d3d4d175f28363dd3869ae16814c1e12f97391
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-02 17:10:21 +0300

    Simplified the logic

---
+++ part6/6.3-6.8/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers/anecdoteReducer'
+import App from './App'
+
+const store = createStore(reducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
