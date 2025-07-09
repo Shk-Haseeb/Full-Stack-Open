55f5f093a2a2c4ab8a1179fdb55a165792e66491
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-09 21:53:09 +0300

    8.3: it works now

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -106,10 +106,18 @@ const typeDefs = `
     id: ID!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -118,6 +126,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length,
   }
 }
 
