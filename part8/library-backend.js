730a7fa9bf765e447a44d04359b2a1780262baa6
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-09 22:04:26 +0300

    8.4: success!

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -93,9 +93,6 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
   type Book {
@@ -116,7 +113,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -125,7 +122,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors,
   },
   Author: {
